2fdf306a0c9f1a351a0a5386212f76e67e7f25fa
ossi arola <ossi.arola@solidabis.com>
2023-04-19 13:26:41 +0300

    randomize playing order

--- app/components/GameBoard.tsx
+++ app/components/GameBoard.tsx
@@ -17,6 +17,8 @@ export default function GameBoard({}: Props) {
 
     const [turn, setTurn] = React.useState<'X' | 'O'>('X')
     const [winner, setWinner] = React.useState<'X' | 'O' | 'Draw' | null>(null)
+    const [playerCharacter, setPlayerCharacter] = React.useState<'X' | 'O' | null>(null)
+    const [computerCharacter, setComputerCharacter] = React.useState<'X' | 'O' | null>(null)
 
     useEffect(() => {
       if (winner) {
@@ -60,25 +62,41 @@ export default function GameBoard({}: Props) {
           return
         }
       }
+
+      const randomizePlayerCharacter = () => {
+        return ['X', 'O'][Math.floor(Math.random() * 2)] as 'X' | 'O'
+      }
+
+      if (!playerCharacter) {
+        setPlayerCharacter(randomizePlayerCharacter)
+        return
+      }
+
+      if (!computerCharacter) {
+        setComputerCharacter(playerCharacter === 'X' ? 'O' : 'X')
+        return
+      }
+
       
-      if (turn === 'X') {
+      // if (turn === 'X') {
+      if (turn === playerCharacter && !winner) {
         checkWinner()
         document.title = 'Tic Tac Toe | X'
       }
-      else if (turn === 'O' && !winner)  {
+      else if (turn === computerCharacter && !winner)  {
         checkWinner()
         document.title = 'Tic Tac Toe | O'
         const newGrid = [...grid]
         const computerMove = computerTurn(newGrid)
         console.log(computerMove)
         if (computerMove) {
-          newGrid[computerMove[0]][computerMove[1]] = turn
+          newGrid[computerMove[0]][computerMove[1]] = computerCharacter
           setGrid(newGrid)
-          setTurn('X')
+          setTurn(playerCharacter)
         }
       }
       
-      }, [turn, grid, winner])
+    }, [turn, grid, winner, playerCharacter, computerCharacter])
 
     function gameClick(rowIndex:number, colIndex:number) {
       const newGrid = [...grid]
@@ -90,7 +108,7 @@ export default function GameBoard({}: Props) {
     if (winner) {
       return (
         <div className='w-max m-auto pt-10'>
-          <h1 className='text-3xl font-bold text-center p-6'>{winner === 'Draw' ? 'Draw' : `${winner} is the winner!`}</h1>
+          <h1 className='text-3xl font-bold text-center p-6'>{winner === 'Draw' ? 'Draw' : winner === playerCharacter ? 'You Win' : 'You Lose'}!</h1>
           <button className='w-max m-auto p-2 bg-purple-500 text-white font-bold rounded-md' onClick={() => window.location.reload()}>Play Again</button>
         </div>
       )
@@ -98,23 +116,27 @@ export default function GameBoard({}: Props) {
 
 
   return (
-    <div className='grid grid-cols-3 grid-rows-3 gap-0 w-max m-auto pt-10'>
-        {grid.map((row, rowIndex) => (
-          <div key={rowIndex}>
-            {row.map((col, colIndex) => (
-              <div key={colIndex} className='w-28 h-28 p-2 border-2 border-red-500 text-center font-bold text-3xl flex justify-center items-center'>
-                {col === 0 ? <div className='cursor-pointer w-full h-full' onClick={() =>gameClick(rowIndex, colIndex)}> </div>
-                : 
-                <div className={col === 'X' ? 'text-blue-500 cursor-default' : 'text-fuchsia-500 cursor-default'}>
-                  {col}
-                </div>}
-              </div>
-            ))}
+    <>
+      <h2>You play as {playerCharacter}</h2>
+      <h2>Computer plays as {computerCharacter}</h2>
+      <div className='grid grid-cols-3 grid-rows-3 gap-0 w-max m-auto pt-10'>
+          {grid.map((row, rowIndex) => (
+            <div key={rowIndex}>
+              {row.map((col, colIndex) => (
+                <div key={colIndex} className='w-28 h-28 p-2 border-2 border-red-500 text-center font-bold text-3xl flex justify-center items-center'>
+                  {col === 0 ? <div className='cursor-pointer w-full h-full' onClick={() =>gameClick(rowIndex, colIndex)}> </div>
+                  : 
+                  <div className={col === 'X' ? 'text-blue-500 cursor-default' : 'text-fuchsia-500 cursor-default'}>
+                    {col}
+                  </div>}
+                </div>
+              ))}
 
-          </div>
-        )  
-        )}
-        
-    </div>
+            </div>
+          )  
+          )}
+          
+      </div>
+    </>
   )
 }
