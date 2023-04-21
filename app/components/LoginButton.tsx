685bf1e6cafa3945c23c50eeba7ab98eb958afbc
ossi arola <ossi.arola@solidabis.com>
2023-04-21 15:31:49 +0300

    added shadow on profile button

--- app/components/LoginButton.tsx
+++ app/components/LoginButton.tsx
@@ -8,7 +8,7 @@ const LoginButton = (props: Props) => {
 const { data: session } = useSession();
 if (session) {
     return (
-        <div className="dropdown cursor-pointer bg-slate-700 self-center pl-4 pr-4 inline-block relative w-24 rounded-md hover:rounded-b-none transition-all ease-linear duration-300">
+        <div className="dropdown cursor-pointer shadow-black shadow-md bg-slate-700 self-center pl-4 pr-4 inline-block relative w-24 rounded-md hover:rounded-b-none transition-all ease-linear duration-300">
             {session.user?.name}
             <div className="dropdown-content bg-slate-700 hidden absolute z-10 right-0 w-24 rounded-b hover:font-bold hover:underline">
                 <button className=" block w-20 m-auto" onClick={() => signOut()}>Log out</button>
